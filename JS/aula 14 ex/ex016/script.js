34a9bf90c24b01ce6c9dcd70584b264c87263a50
Bruno Montich <montich.bruno@gmail.com>
2021-06-15 16:38:32 -0300

    ex 16

---
+++ JS/aula 14 ex/ex016/script.js
@@ -0,0 +1,12 @@
+var ini = document.getElementById('inicio')
+var fim = document.getElementById('final')
+var passo = document.getElementById('passo')
+var res = document.getElementById('res')
+
+function contar(){
+    if(ini >= 0){
+        
+    } else{
+    res.innerHTML = 'Impossível contar'
+    }
+}
